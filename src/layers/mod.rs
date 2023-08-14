4876cad80827fb99bd57a5de0896a288c683de42
Lennart <18233294+lennart-k@users.noreply.github.com>
2023-08-14 19:36:07 +0200

    Refactoring layers

--- src/layers/mod.rs
+++ src/layers/mod.rs
@@ -1,2 +1,27 @@
+use crate::common::GenericData;
+use crate::layer::RunGeneric;
+use anyhow::Result;
+use async_trait::async_trait;
+use fetch_http::FetchHttpLayer;
+use regex_find::RegexFindLayer;
+use serde::Deserialize;
+
 pub mod fetch_http;
 pub mod regex_find;
+
+#[derive(Deserialize, Debug)]
+#[serde(tag = "type")]
+pub enum GenericLayer {
+    FetchHttp(FetchHttpLayer),
+    RegexFind(RegexFindLayer),
+}
+
+#[async_trait]
+impl RunGeneric for GenericLayer {
+    async fn run_generic(&self, out: &GenericData) -> Result<GenericData> {
+        match self {
+            GenericLayer::FetchHttp(builder) => builder.run_generic(out).await,
+            GenericLayer::RegexFind(builder) => builder.run_generic(out).await,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use anyhow::Result;
-use async_trait::async_trait;
 use common::GenericData;
 use layer::RunGeneric;
-use layers::fetch_http::FetchHttpLayer;
-use layers::regex_find::RegexFindLayer;
-use serde::Deserialize;
+use layers::GenericLayer;
 use std::fs;
 
 pub mod argument;
@@ -12,23 +9,6 @@ pub mod common;
 pub mod layer;
 pub mod layers;
 pub mod scope;
-
-#[derive(Deserialize, Debug)]
-#[serde(tag = "type")]
-enum GenericLayer {
-    FetchHttp(FetchHttpLayer),
-    RegexFind(RegexFindLayer),
-}
-
-#[async_trait]
-impl RunGeneric for GenericLayer {
-    async fn run_generic(&self, out: &GenericData) -> Result<GenericData> {
-        match self {
-            GenericLayer::FetchHttp(builder) => builder.run_generic(out).await,
-            GenericLayer::RegexFind(builder) => builder.run_generic(out).await,
-        }
-    }
-}
 
 #[tokio::main]
 async fn main() -> Result<()> {
